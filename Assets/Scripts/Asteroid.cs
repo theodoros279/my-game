412d738a39b62d09461f8dcd36338d09549c3066
theodoros279 <47207136+theodoros279@users.noreply.github.com>
2022-02-11 19:15:06 +0000

    destroy asteroids

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,8 +4,10 @@ public class Asteroid : MonoBehaviour
 {
     public Sprite[] sprites; 
     public float size = 1.0f;
-    public float minSize = 0.5f;
-    public float maxSize = 1.5f; 
+    public float minSize = 0.4f;
+    public float maxSize = 1.0f; 
+    public float speed = 50.0f;
+    public float maxLifetime = 30.0f;
 
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rigidbody;
@@ -16,7 +18,8 @@ public class Asteroid : MonoBehaviour
         rigidbody = GetComponent<Rigidbody2D>();
     } 
 
-    private void Start() {
+    private void Start() 
+    {
         spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
 
         this.transform.eulerAngles = new Vector3(0.0f, 0.0f, Random.value * 360.0f);
@@ -24,4 +27,31 @@ public class Asteroid : MonoBehaviour
 
         rigidbody.mass = this.size;
     } 
+
+    public void SetTrojectory(Vector2 direction) 
+    {
+        rigidbody.AddForce(direction * speed); 
+        Destroy(this.gameObject, maxLifetime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if (collision.gameObject.tag == "Bullet") {
+            if ((this.size * 0.5f) >= this.minSize) {
+                SplitAsteroid();
+                SplitAsteroid();
+            }
+            Destroy(this.gameObject);
+        }
+        Debug.Log("hit"); 
+    } 
+
+    private void SplitAsteroid() {
+        Vector2 position = this.transform.position;
+        position +=  Random.insideUnitCircle * 0.5f;
+
+        Asteroid half = Instantiate(this, position, this.transform.rotation);
+        half.size = this.size * 0.5f;
+        half.SetTrojectory(Random.insideUnitCircle.normalized * this.speed); 
+    }
 }
